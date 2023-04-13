c36e6554cecb7194b2ea084aba4dc03f529a2484
荒井梨菜 <>
2023-04-13 22:16:18 +0900

    "/"のルーティング設定

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('welcome');
+});
+// Route::get('/home', 'HomeController@index')->name('home');
 
 //Auth::routes();
 
