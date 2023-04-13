5e220fad0dfa9200badeed586864e9ab91ea7e73
荒井梨菜 <>
2023-04-13 21:20:09 +0900

    Usersのseederファイルの作成、初期ユーザーの設定

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => '初期ユーザー',
+            'mail' => 'user1@gmail.com',
+            'password' => 'user1',
+            'bio' => 'これは初期ユーザーです。初めまして。',
+        ]);
+    }
+}
